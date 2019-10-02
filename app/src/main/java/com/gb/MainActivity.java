9ab4abf3ab1388fc8a8eca547ae6a13d7894e1bb
nayyar <nayyar.syed@gmail.com>
2019-10-02 21:59:34 +1300

    addded a dynamic button and xml button on frame layout and set the position of dynamic button as well and its dimensions.

--- app/src/main/java/com/gb/MainActivity.java
+++ app/src/main/java/com/gb/MainActivity.java
@@ -6,28 +6,36 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    public Button button ;
+    public Button b1 ;
     public FrameLayout jfl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.butt);
         jfl = findViewById(R.id.fl);
         final BouncingBallInside bouncingballi = new BouncingBallInside(this);
         jfl.addView(bouncingballi);
+        b1= new Button(this);
+       // b1 = findViewById(R.id.butt);
+        b1.setLayoutParams(new LinearLayout.LayoutParams(400,200));
+        b1.setX(300);
+        b1.setY(400);
+        b1.setText( "Dynamic button 2 Recreate activity " );
+        //Re creating teh Activity
+        jfl.addView( b1 );
 
-        button.setOnClickListener(new View.OnClickListener() {
+        b1.setOnClickListener( new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-
-
+            public void onClick(View b1_listner) {
+                recreate();
             }
-        });
+        } );
+
 
 
     }
